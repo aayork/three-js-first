2c59edb33cc534c0c63d944d364ede686d871d26
Aidan York <113198662+aayork@users.noreply.github.com>
2024-05-28 17:14:49 -0400

    add new scans

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000,
 );
-camera.position.z = 5.0;
-camera.position.y = 4.0;
-camera.position.x = -2.0;
+camera.position.z = 2.0;
+camera.position.y = 2.0;
+camera.position.x = 0.0;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -48,30 +48,30 @@ function loadGLTF(url, position, rotation, scale) {
 }
 
 // Load the chairs and rug with appropriate positions and scales
-loadGLTF("/chair.glb", { x: -1, y: -1.25, z: 0 }, null, {
-  x: 1.0,
-  y: 1.0,
-  z: 1.0,
+loadGLTF("/chair.glb", { x: -1.5, y: -1.25, z: 0 }, null, {
+  x: 6.0,
+  y: 6.0,
+  z: 6.0,
 });
 
 loadGLTF(
   "/chair.glb",
-  { x: 2, y: -1.25, z: 0 },
+  { x: 1, y: -1.25, z: 0 },
   { x: 0, y: 3, z: 0 },
-  { x: 1.0, y: 1.0, z: 1.0 },
+  { x: 6.0, y: 6.0, z: 6.0 },
 );
 
 loadGLTF(
   "/chair.glb",
   { x: 0, y: -1.25, z: 0.7 },
   { x: 0, y: 7, z: 0 },
-  { x: 1.0, y: 1.0, z: 1.0 },
+  { x: 6.0, y: 6.0, z: 6.0 },
 );
 
 loadGLTF("/rug.glb", { x: 0, y: -1.2, z: 0 }, null, {
-  x: 1.0,
-  y: 1.0,
-  z: 1.0,
+  x: 6.0,
+  y: 6.0,
+  z: 6.0,
 });
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
@@ -95,10 +95,10 @@ const viewer = new GaussianSplats3D.Viewer({
 viewer
   .addSplatScenes([
     {
-      path: "/bigger_room.ply",
-      position: [0, 0.85, 0],
+      path: "/bigger_room.splat",
+      position: [-5, 9, -10],
       rotation: [1, 0, 0, 0],
-      scale: [0.5, 0.5, 0.5],
+      scale: [2.5, 2.5, 2.5],
     },
   ])
   .then(() => {
